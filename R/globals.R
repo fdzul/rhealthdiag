5694f1ad9664bd27c8dacd892288c9a77931dead
Felipe Dzul <felipe.dzul.m@gmail.com>
2021-09-10 20:20:16 -0500

    this commit update the globals

--- R/globals.R
+++ R/globals.R
@@ -1,6 +1,6 @@
 utils::globalVariables(c("anio_ocur", "edad_des", "ent_ocurr","lista_mex_des",
                          "lista_mex_cve",
                          "loc_ocurr", "mun_ocurr", "n", "total",
-                         "CVE_ENT", "CVE_MUN", "TIPE_C", "value", "NOMGEO",
+                         "CVE_ENT", "CVE_MUN", "TIPO_C", "value", "NOMGEO",
                          "entidad", "mun", "nom_loc", "altitud", "pobtot",
-                         "latitud", "longitud", "loc"))
+                         "latitude", "longitude", "loc", "iter_2020_loc", "nom_mun"))
